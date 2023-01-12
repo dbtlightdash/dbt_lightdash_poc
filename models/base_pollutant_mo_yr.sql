46d74a9f07226e4e6d4813c30746ccef9024fce6
Adi Manohar <dbtlightdash@gmail.com>
2023-01-12 16:35:45 -0500

    bug fixed air quality data

--- models/base_pollutant_mo_yr.sql
+++ models/base_pollutant_mo_yr.sql
@@ -1,4 +1,7 @@
-SELECT country, pollutant, AVG(value)  
+SELECT 
+country
+, pollutant
+, AVG(value) as average_val
 ,EXTRACT(MONTH FROM timestamp) AS MONTH
 , EXTRACT(YEAR FROM timestamp) AS YEAR
 FROM `dulcet-answer-373817.dbt_ld.global_air_quality` 
--- models/tgt_pollutant_mo_yr.sql
+++ models/tgt_pollutant_mo_yr.sql
@@ -5,6 +5,5 @@ FROM `dulcet-answer-373817.dbt_ld.country_codes`
 
 SELECT *
 FROM {{ ref('pollutant_avg_yr') }}
-LEFT JOIN 
-on country_key.alpha_2_code = pollutant_avg_yr.country
+LEFT JOIN country_key ON country_key.alpha_2_code = pollutant_avg_yr.country
 
